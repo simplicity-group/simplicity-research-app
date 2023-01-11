5fe8bfab8b696d6d9ccebaddcb5cbf2177ab426a
Dev-Betterverse <117495872+LemonHayds@users.noreply.github.com>
2023-01-11 23:22:34 +0000

    fix dependencies

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -32,14 +32,14 @@ export default function Navbar() {
     }
   }, [user])
 
-  if (window.location.pathname === '/'){
+  if (window.location.pathname === '/' || ''){
     return null
   } else {
     return (
       <Disclosure as="nav" className="bg-white-800 shadow">
         {({ open }) => (
           <>
-            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className=" px-2 sm:px-6 lg:px-8">
               <div className="flex relative h-16 items-center justify-between">
                 <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                   {/* Mobile menu button*/}
@@ -93,7 +93,7 @@ export default function Navbar() {
                       <svg className="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
                     </div>
-                      <input type="text" id="search-navbar" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search"/>
+                      <input type="text" id="search-navbar" className="bg-gray-100 block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500" placeholder="Search"/>
                   </div>
                   {/* Profile dropdown */}
                   <Menu as="div" className="relative ml-3 h-full">
@@ -155,7 +155,7 @@ export default function Navbar() {
                     <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                       <svg className="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
                     </div>
-                    <input type="text" id="search-navbar" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search "/>
+                    <input type="text" id="search-navbar" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search "/>
                   </div>
                 </div>
               </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
 // tailwind.config.js
 const defaultTheme = require('tailwindcss/defaultTheme')
-module.exports = {
-   purge: [],
-   theme: {
-     extend: {
-        fontFamily: {
-           sans: ['montserrat', ...defaultTheme.fontFamily.sans],
-        },
-     },
-   },
-  variants: {},
-  plugins: [require("@tailwindcss/forms")],
 
-};
+module.exports = {
+   purge: [
+     'src/**/*.js',
+     'src/**/*.jsx',
+     'src/**/*.ts',
+     'src/**/*.tsx',
+     'public/**/*.html',
+   ],
+   theme: {
+      extend: {
+         fontFamily: {
+            sans: ['montserrat', ...defaultTheme.fontFamily.sans],
+         },
+      },
+    },
+   variants: {},
+   plugins: [require("@tailwindcss/forms")],
+}
