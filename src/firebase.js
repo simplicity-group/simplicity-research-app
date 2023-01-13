dc9d8f8f9f0aa47ecbf8bd3c3694de9b828801db
Dan Miles <dan@slide.so>
2023-01-13 16:37:30 +0000

    update firebase config to point at prod

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,20 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth, updateProfile } from "firebase/auth";
-import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+import {initializeApp} from "firebase/app";
+import {getAuth, updateProfile} from "firebase/auth";
+import {getDownloadURL, getStorage, ref, uploadBytes} from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBgiZ9urXTQekalMq6Xjys4EqJsPoYhEoA",
-  authDomain: "basic-auth-3209c.firebaseapp.com",
-  projectId: "basic-auth-3209c",
-  storageBucket: "basic-auth-3209c.appspot.com",
-  messagingSenderId: "468098529277",
-  appId: "1:468098529277:web:e1f55c54a4d38192f0cb94"
+    apiKey: "AIzaSyAUxuAkhANfbtDgf1xfDSR5dLVYxHC6dRQ",
+    authDomain: "simplicity-research-app.firebaseapp.com",
+    projectId: "simplicity-research-app",
+    storageBucket: "simplicity-research-app.appspot.com",
+    messagingSenderId: "675528585352",
+    appId: "1:675528585352:web:07284e7942db12d8fd2f3d",
+    measurementId: "G-NRZ36QV40E"
 };
 
 // Initialize Firebase
@@ -22,15 +23,15 @@ const storage = getStorage();
 
 export const auth = getAuth(app);
 
-export async function upload(file, currentUser, setLoading){
-  const fileRef = ref(storage, currentUser.uid);
-  setLoading(true);
-  const snapshot = await uploadBytes(fileRef, file);
-  const photoURL = await getDownloadURL(fileRef);
-  updateProfile(currentUser, {photoURL})
+export async function upload(file, currentUser, setLoading) {
+    const fileRef = ref(storage, currentUser.uid);
+    setLoading(true);
+    const snapshot = await uploadBytes(fileRef, file);
+    const photoURL = await getDownloadURL(fileRef);
+    updateProfile(currentUser, {photoURL})
 
-  setLoading(false);
-  alert('uploaded');
+    setLoading(false);
+    alert('uploaded');
 
 }
 
