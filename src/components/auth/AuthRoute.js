06ab99bc51ccb9d9261ef6ddf6300a1d6d454509
Dev-Betterverse <117495872+LemonHayds@users.noreply.github.com>
2023-02-06 11:20:26 +0000

    loading auth fix

--- src/components/auth/AuthRoute.js
+++ src/components/auth/AuthRoute.js
@@ -1,6 +1,7 @@
 import React, { Children } from 'react'
 import { Navigate } from 'react-router-dom'
 import { UserAuth } from '../../context/AuthContext'
+import LoadingAuth from '../general/LoadingAuth';
 
 const AuthRoute = ({children}) => {
     const {user, userComplete} = UserAuth();
@@ -10,7 +11,12 @@ const AuthRoute = ({children}) => {
         return <Navigate to='/home' />
       }
       else{
-        return <Navigate to='/accountsetup' />
+        if(userComplete === null){
+          return <LoadingAuth />  
+        }
+        else{
+          return <Navigate to='/accountsetup' />
+        }
       }
     }
 
